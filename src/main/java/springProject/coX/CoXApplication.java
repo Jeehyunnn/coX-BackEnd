0ed64f02831ee2c12a4b63063c8b36baf11061ba
Jeehyunnn <luuvwindy@gmail.com>
2024-08-09 17:33:20 +0900

    Initial commit

---
+++ src/main/java/springProject/coX/CoXApplication.java
@@ -0,0 +1,13 @@
+package springProject.coX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoXApplication.class, args);
+	}
+
+}
